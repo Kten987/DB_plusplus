379b03775fe8be26a6cd1a45ecc3e4230a766613
Kten987 <82595121+Kten987@users.noreply.github.com>
2021-11-10 15:32:07 +0700

    Update Buoi_4.1.sql

--- Buoi_4.1.sql
+++ Buoi_4.1.sql
@@ -4,5 +4,5 @@ select * from laptop order by price asc limit 5 ;
 -- 2. Trả ra các hãng sản xuất laptop và sắp xếp theo thứ tự Alphabet.
 select distinct maker from laptop order by maker asc;
 -- 3. Viết câu query trả ra doanh số laptop bán được và doanh thu của cửa hàng.
-select sold, sold*price "Doanh thu" from laptop;
+select sum(sold), sum(sold*price) "Doanh thu" from laptop;
 
