0c9cc73e43367e3b76229587f6316d98f6d8b8ab
Kten987 <82595121+Kten987@users.noreply.github.com>
2021-11-21 09:37:29 +0700

    Add files via upload

---
+++ Buoi_5.1.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user='root',
+    password='Taolaisowa123',
+    database="store_cms_plusplus"
+)
+mycursor = mydb.cursor()
+
+def insert(input_name,input_link,input_maker,input_type):
+    str2 = f"insert into laptop (name,url,maker,type) values ('{input_name}','{input_link}','{input_maker}','{input_type}');"
+    mycursor.execute(str2)
+    myresult1 = mycursor.fetchall()
+    print(myresult1)
+
+def search():
+    print("Mời bạn nhập tên sản phẩm cần tìm: ")
+    search_name = input()
+    str1 = f"select * from laptop where name = '{search_name}' ;"
+    mycursor.execute(str1)
+    myresult = mycursor.fetchall()
+    print(myresult)
+
+insert('thinkpad','abc','lenovo','abc')
+search()
---
+++ Buoi_5.2.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user='root',
+    password='Taolaisowa123',
+    database="store_cms_plusplus"
+)
+mycursor = mydb.cursor()
+
+def update_sold(id,up_sold):
+    str = f"update laptop set sold = '{up_sold}' WHERE id='{id}';"
+    mycursor.execute(str)
+    mycursor.execute(f"select * from laptop where id = '{id}';")
+    myresult = mycursor.fetchall()
+    print(myresult)
+
+update_sold('6',"120")
